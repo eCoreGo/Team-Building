56e145641719c3093cee644087be2fd475b2e0f2
stereomatrix <840933659@qq.com>
2016-03-25 19:18:20 +0800

    Update database design.

--- src/main/java/com/core/bean/ActivityAttender.java
+++ src/main/java/com/core/bean/ActivityAttender.java
@@ -7,4 +7,28 @@ public class ActivityAttender {
     private Integer activityId;
     private Integer userId;
     private Integer seats;
+
+    public Integer getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(Integer activityId) {
+        this.activityId = activityId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getSeats() {
+        return seats;
+    }
+
+    public void setSeats(Integer seats) {
+        this.seats = seats;
+    }
 }
--- src/main/java/com/core/bean/TeamMember.java
+++ src/main/java/com/core/bean/TeamMember.java
@@ -10,4 +10,36 @@ public class TeamMember {
     private Team team;
     private Double balance;
     private Date attendTime;
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Date getAttendTime() {
+        return attendTime;
+    }
+
+    public void setAttendTime(Date attendTime) {
+        this.attendTime = attendTime;
+    }
 }
