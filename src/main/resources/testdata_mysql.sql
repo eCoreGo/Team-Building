f78293804481c6313c977b1ebc1189a917f8a71d
stereomatrix <840933659@qq.com>
2016-03-28 23:35:27 +0800

    Fix the issue to display teams.

--- src/main/resources/testdata_mysql.sql
+++ src/main/resources/testdata_mysql.sql
@@ -1,5 +1,5 @@
 insert into member(id, name, wechat_no, phone) values(1, 'user1', '1', '12345678');
-insert into member(id, name, wechat_no, phone) values(2, 'user1', '2', '87654321');
+insert into member(id, name, wechat_no, phone) values(2, 'user2', '2', '87654321');
 
 insert into team(id, temp, name, description, total_foundation, total_user_balance, creation_time) values(1, false, 'eCore', 'A wonderful team!', 100, 200, '2016/03/20 11:11:11');
 insert into team(id, temp, name, description, total_foundation, total_user_balance, creation_time) values(2, false, 'CCAR', 'ABC!', 100, 200, '2016/03/20 11:11:11');
