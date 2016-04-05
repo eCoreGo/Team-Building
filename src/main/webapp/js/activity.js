85bee17c57242154bf24f60d7cc8a4db462fe112
stereomatrix <840933659@qq.com>
2016-04-05 21:46:00 +0800

    Add features.

--- src/main/webapp/js/activity.js
+++ src/main/webapp/js/activity.js
@@ -11,21 +11,24 @@ $(document).on("pageshow", function() {
         },
         success: function(result) {
             $("#activitiesListView").empty();
-            for(var i = 0; i < result.length; i++) {
-                $("#activitiesListView").append(function() {
-                    return "<li>" +
-                        "<a data-ajax = 'false' href='activity-item.html?id=" + result[i].id + "'>" +
-                        "<h1>" + result[i].name + "</h1>" +
-                        "<p><strong>" + result[i].startTime + "-" + result[i].endTime + "</strong></p>" +
-                        "<p>" + result[i].description + "</p>" +
-                        "<p class='ui-li-aside'><strong>" + result[i].status + "</strong></p>" +
-                        "</a></li>";
-                });
+            if(result.length == 0) {
+                $("#activitiesListView").append("<li><a href='javascript:void(0);'>No record!</a></li>");
+            } else {
+                for(var i = 0; i < result.length; i++) {
+                    $("#activitiesListView").append(function() {
+                        return "<li>" +
+                            "<a data-ajax = 'false' href='activity-item.html?id=" + result[i].id + "'>" +
+                            "<h1>" + result[i].name + "</h1>" +
+                            "<p><strong>" + result[i].startTime + "-" + result[i].endTime + "</strong></p>" +
+                            "<p>" + result[i].description + "</p>" +
+                            "<p class='ui-li-aside'><strong>" + result[i].status + "</strong></p>" +
+                            "</a></li>";
+                    });
+                }
             }
-            $("#activitiesListView").selectmenu("refresh");
         },
         complete: function() {
-            $("#teamList").listview("refresh");
+            $("#activitiesListView").listview("refresh");
         }
     });
 });
