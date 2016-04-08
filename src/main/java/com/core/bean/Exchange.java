65ae95adb349ce9f1724708c4539946ee08efc38
stereomatrix <840933659@qq.com>
2016-04-09 00:50:12 +0800

    Update database.

---
+++ src/main/java/com/core/bean/Exchange.java
@@ -0,0 +1,72 @@
+package com.core.bean;
+
+import java.util.Date;
+
+/**
+ * Created by stereomatrix on 2016/4/9.
+ */
+public class Exchange {
+    private Integer id;
+    private Member member;
+    private Activity activity;
+    private Team team;
+    private Double value;
+    private Date date;
+    private Integer type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+}
--- src/main/resources/testdata_mysql.sql
+++ src/main/resources/testdata_mysql.sql
@@ -7,6 +7,8 @@ insert into team(id, temp, name, description, total_foundation, total_user_balan
 insert into team_member(team_id, member_id, balance, attend_time) values(1, 1, 100, '2016/03/20 11:11:11');
 insert into team_member(team_id, member_id, balance, attend_time) values(2, 2, 150, '2016/03/20 11:11:11');
 
-insert into activity(id, name, total_cost, total_foundation_cost, description, status, start_time, end_time, team_id) values(1, "ÉÕ¿¾1", 2000, 1000, "desc", 1, '2016/03/20 11:11:11', '2016/03/24 11:11:11', 1);
-insert into activity(id, name, total_cost, total_foundation_cost, description, status, start_time, end_time, team_id) values(2, "ÉÕ¿¾2", 2200, 1200, "desc", 2, '2016/03/20 11:11:11', '2016/03/24 11:11:11', 1);
-insert into activity(id, name, total_cost, total_foundation_cost, description, status, start_time, end_time, team_id) values(3, "ÉÕ¿¾3", 2400, 1400, "desc", 3, '2016/03/20 11:11:11', '2016/03/24 11:11:11', 2);
+insert into activity(id, name, total_cost, total_foundation_cost, description, status, start_time, end_time, team_id) values(1, "çƒ§çƒ¤1", 2000, 1000, "desc", 1, '2016/03/20 11:11:11', '2016/03/24 11:11:11', 1);
+insert into activity(id, name, total_cost, total_foundation_cost, description, status, start_time, end_time, team_id) values(2, "çƒ§çƒ¤2", 2200, 1200, "desc", 2, '2016/03/20 11:11:11', '2016/03/24 11:11:11', 1);
+insert into activity(id, name, total_cost, total_foundation_cost, description, status, start_time, end_time, team_id) values(3, "çƒ§çƒ¤3", 2400, 1400, "desc", 3, '2016/03/20 11:11:11', '2016/03/24 11:11:11', 2);
+
+insert into exchange_detail(id, member_id, activity_id, team_id, exchange, exchange_status, date, team_total, expired) values(1, 1, 1, 1, 200, 2, '2016/03/20 11:11:11', 500, false);
