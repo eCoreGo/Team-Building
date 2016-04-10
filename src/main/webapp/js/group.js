c52a5ec772fcbd654e67674da41ee85726478c60
WENZI <WENZI@WENZI-PC>
2016-04-10 15:58:00 +0800

    add userId 4 link

--- src/main/webapp/js/group.js
+++ src/main/webapp/js/group.js
@@ -2,6 +2,19 @@
  * Created by stereomatrix on 2016/3/25.
  */
 $(document).on("pageshow", function() {
+	var userId = findParameterValue("userId");
+	
+	var activityLinkUrl = $("#activityLink").attr("href");
+	var groupLinkUrl = $("#groupLink").attr("href");
+	var mineLinkUrl = $("#mineLink").attr("href");
+	var groupAddLinkUrl = $("groupAddLink").attr("href");
+	var groupSearchLinkUrl = $("groupSearchLink").attr("href");
+    $("#activityLink").attr("href", activityLinkUrl + "?userId=" + userId);
+    $("#groupLink").attr("href", groupLinkUrl + "?userId=" + userId);
+    $("#mineLink").attr("href", mineLinkUrl + "?userId=" + userId);
+    $("#groupAddLink").attr("href", groupAddLinkUrl + "?userId=" + userId);
+    $("#groupSearchLink").attr("href", groupSearchLinkUrl + "?userId=" + userId);
+	
     $.ajax({
         type: "POST",
         dataType: "json",
@@ -10,7 +23,7 @@ $(document).on("pageshow", function() {
             $("#teamList").empty();
             for(var i = 0; i < result.length; i++) {
                 $("#teamList").append(function() {
-                    return "<li><a data-ajax = 'false' href='group-item.html?id=" + result[i].id + "'>" + result[i].name + "</a></li>";
+                    return "<li><a data-ajax = 'false' href='group-item.html?userId=" + userId + "&id=" + result[i].id + "'>" + result[i].name + "</a></li>";
                 });
             }
         },
@@ -19,3 +32,10 @@ $(document).on("pageshow", function() {
         }
     });
 });
+
+function findParameterValue(parameterKey) {
+	var raw = window.location.search;
+    var queryString = raw.substr(1);
+    var queryParameterMap = parseQueryString(queryString);
+    return queryParameterMap[parameterKey];
+}
