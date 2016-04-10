827e788aa2546a9896e87251f10c9fa1040040a4
WENZI <WENZI@WENZI-PC>
2016-04-11 01:30:57 +0800

    add userId

--- src/main/webapp/js/group.js
+++ src/main/webapp/js/group.js
@@ -8,7 +8,7 @@ $(document).on("pageshow", function() {
 	var groupLinkUrl = $("#groupLink").attr("href");
 	var mineLinkUrl = $("#mineLink").attr("href");
 	var groupAddLinkUrl = $("#groupAddLink").attr("href");
-	var groupSearchLinkUrl = $("groupSearchLink").attr("href");
+	var groupSearchLinkUrl = $("#groupSearchLink").attr("href");
     $("#activityLink").attr("href", activityLinkUrl + "?userId=" + userId);
     $("#groupLink").attr("href", groupLinkUrl + "?userId=" + userId);
     $("#mineLink").attr("href", mineLinkUrl + "?userId=" + userId);
