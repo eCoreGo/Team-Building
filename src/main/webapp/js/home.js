c28bdbb5d221f330433f70af822f4805d1f8a841
WENZI <WENZI@WENZI-PC>
2016-04-11 01:48:47 +0800

    add userId

---
+++ src/main/webapp/js/home.js
@@ -0,0 +1,10 @@
+$(document).on("pageshow", function() {
+	var userId = findParameterValue("userId");
+	
+	var activityLinkUrl = $("#activityLink").attr("href");
+	var groupLinkUrl = $("#groupLink").attr("href");
+	var mineLinkUrl = $("#mineLink").attr("href");
+    $("#activityLink").attr("href", activityLinkUrl + "?userId=" + userId);
+    $("#groupLink").attr("href", groupLinkUrl + "?userId=" + userId);
+    $("#mineLink").attr("href", mineLinkUrl + "?userId=" + userId);
+});
