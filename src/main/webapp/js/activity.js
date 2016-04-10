2a941857ebd27bb8feeb0d2bc36a6ff284270e8e
WENZI <WENZI@WENZI-PC>
2016-04-11 02:03:23 +0800

    add userId

--- src/main/webapp/js/activity.js
+++ src/main/webapp/js/activity.js
@@ -1,7 +1,19 @@
 /**
  * Created by stereomatrix on 2016/3/25.
  */
+
+var userId;
 $(document).on("pageshow", function() {
+	userId = findParameterValue("userId");
+	
+	var activityLinkUrl = $("#activityLink").attr("href");
+	var groupLinkUrl = $("#groupLink").attr("href");
+	var mineLinkUrl = $("#mineLink").attr("href");
+	
+	$("#activityLink").attr("href", activityLinkUrl + "?userId=" + userId);
+    $("#groupLink").attr("href", groupLinkUrl + "?userId=" + userId);
+    $("#mineLink").attr("href", mineLinkUrl + "?userId=" + userId);
+    
     getActivitiesByStatus();
     $("fieldset input").bind("change", function() {
         getActivitiesByStatus();
@@ -46,4 +58,21 @@ function getActivitiesByStatus(status) {
             $("#activitiesListView").listview("refresh");
         }
     });
+}
+
+function findParameterValue(parameterKey) {
+	var raw = window.location.search;
+    var queryString = raw.substr(1);
+    var queryParameterMap = parseQueryString(queryString);
+    return queryParameterMap[parameterKey];
+}
+
+function parseQueryString(queryString) {
+    var params = queryString.split("&");
+    var temp, query = {};
+    for(var i = 0, l = params.length; i < l; i++) {
+        temp = params[i].split("=");
+        query[temp[0]] = temp[1];
+    }
+    return query;
 }
