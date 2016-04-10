23f050389bae96d71092bbb65e8faa9357267dd4
WENZI <WENZI@WENZI-PC>
2016-04-10 17:20:30 +0800

    add userId 4 link in mine page

--- src/main/webapp/js/mine.js
+++ src/main/webapp/js/mine.js
@@ -1,10 +1,22 @@
 $(document).on("pageshow", function() {
-        
+	var userId = findParameterValue("userId");
+	
+	var activityLinkUrl = $("#activityLink").attr("href");
+	var groupLinkUrl = $("#groupLink").attr("href");
+	var mineLinkUrl = $("#mineLink").attr("href");
+	var mineFinanceUrl = $("#mineFinance").attr("href");
+	var mineProfileUrl = $("#mineProfile").attr("href");
+    $("#activityLink").attr("href", activityLinkUrl + "?userId=" + userId);
+    $("#groupLink").attr("href", groupLinkUrl + "?userId=" + userId);
+    $("#mineLink").attr("href", mineLinkUrl + "?userId=" + userId);
+    $("#mineFinance").attr("href", mineFinanceUrl + "?userId=" + userId);
+    $("#mineProfile").attr("href", mineProfileUrl + "?userId=" + userId);
+    
     $.ajax({
         type: "POST",
         dataType: "json",
         data: {
-            id: "1"
+            id: userId
         },
         url:"service/Member/getMemberById",
         success: function(result) {
@@ -20,3 +32,20 @@ $(document).on("pageshow", function() {
         }
     });
 });
+
+function findParameterValue(parameterKey) {
+	var raw = window.location.search;
+    var queryString = raw.substr(1);
+    var queryParameterMap = parseQueryString(queryString);
+    return queryParameterMap[parameterKey];
+}
+
+function parseQueryString(queryString) {
+    var params = queryString.split("&amp;");
+    var temp, query = {};
+    for(var i = 0, l = params.length; i < l; i++) {
+        temp = params[i].split("=");
+        query[temp[0]] = temp[1];
+    }
+    return query;
+}
